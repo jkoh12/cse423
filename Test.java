77310ff25565da3d3f267cec4d459ac643bddd18
jkoh12 <jkoh12@asu.edu>
2022-10-18 17:54:13 -0700

    Created a counter that will return 2 if it reaches 2

--- Test.java
+++ Test.java
@@ -2,6 +2,12 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		for (int i = 0; i < 5; i++) {
+			if (i == 2) {
+				System.out.println("Incremented to two!");
+			}
+		}
 	}
 
 }
